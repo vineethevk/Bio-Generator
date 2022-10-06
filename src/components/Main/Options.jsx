6f3e79737d8f2d0a88ff6758c98e148447c0395f
Vineeth <96077560+vineethevk@users.noreply.github.com>
2022-10-06 23:57:28 +0530

    options part css added

--- src/components/Main/Options.jsx
+++ src/components/Main/Options.jsx
@@ -6,9 +6,9 @@ export const Options = () => {
                 <h2>Options</h2>
             </div>
             <div>
-                <label>Name:</label>
+                <label>Name </label>
                 <input type={"text"} />
-                <label>Gender</label>
+                <label> Gender </label>
                 <select>
                     <option>male</option>
                     <option>female</option>
@@ -17,35 +17,35 @@ export const Options = () => {
             </div>
             <div>
                 <input type={"checkbox"} />
-                <label>Location</label>
+                <label> Location </label>
                 <input type={"text"} />
                 <button>Random Location</button>
             </div>
             <div>
                 <input type={"checkbox"} />
-                <label>School</label>
+                <label> School </label>
                 <input type={"text"} />
-                <button>Random School</button><br />
-                <label>Major</label>
+                <button>Random School</button><br /><br />
+                <label> Major </label>
                 <input type={"text"} />
                 <button>Random Major</button>
             </div>
             <div>
                 <input type={"checkbox"} />
-                <label>Occupation</label>
+                <label> Occupation </label>
                 <input type={"text"} />
                 <button>Random Occupation</button>
             </div>
             <div>
                 <input type={"checkbox"} />
-                <label>Religious background</label>
-                <textarea></textarea>
+                <label> Religious background </label><br />
+                <textarea rows={"4"} cols={"50"}></textarea><br />
                 <button>Random Religion</button>
             </div>
             <div>
                 <input type={"checkbox"} />
-                <label>Reason for meeting with missionaries</label>
-                <textarea></textarea>
+                <label>Reason for meeting with missionaries </label><br />
+                <textarea rows={"4"} cols={"50"}></textarea><br />
                 <button>Random Religion</button>
             </div>
         </div>
